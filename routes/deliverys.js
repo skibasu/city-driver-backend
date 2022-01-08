61d253f5f77b7387d81acd75bd84a5e64f882fec
Su <su@MacBook-Pro-Su.local>
2022-01-08 14:25:25 +0100

    set all types of deliverys request

--- routes/deliverys.js
+++ routes/deliverys.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const router = express.Router()
-const { getDeliverys, getDelivery } = require("../controllers/deliverys")
+const {
+    getDeliverys,
+    getDelivery,
+    postDelivery,
+    putDelivery,
+    deleteDelivery,
+} = require("../controllers/deliverys")
 
-router.route("/").get(getDeliverys)
+router.route("/").get(getDeliverys).post(postDelivery)
 
-router.route("/:id").get(getDelivery)
+router.route("/:id").get(getDelivery).put(putDelivery).delete(deleteDelivery)
 
 module.exports = router
