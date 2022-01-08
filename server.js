512dab221bb7951291099abb12eccbf962b620f9
Su <su@MacBook-Pro-Su.local>
2022-01-08 02:01:58 +0100

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" })
+
+const app = express()
+const PORT = process.env.PORT || 6000
+
+app.listen(
+    PORT,
+    console.log(
+        `server is runing in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+)
